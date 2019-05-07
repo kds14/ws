5351dda270fca95decaac47e1af08c2ed4678487
Kyle Stratton <kdstratton@gmail.com>
2019-05-07 14:11:56 -0400

    Added support for multiple connections

--- ws.py
+++ ws.py
@@ -1,4 +1,5 @@
 import socket
+import select
 
 class Req():
 	GET=0x1
@@ -12,16 +13,16 @@ class Request():
 class Router():
 	def route(self, rt, reqs):
 		def set_route(func):
-			self.routes[rt] = (lambda : send_response(self.conn, func()), reqs)
+			self.routes[rt] = (lambda conn: send_response(conn, func()), reqs)
 			return func
 		return set_route
 
-	def handle_route(self, rt, reqs):
+	def handle_route(self, conn, rt, req):
 		# prevent bad reqs
 		if rt in self.routes:
 			r = self.routes[rt]
-			if r[1] & reqs:
-				r[0]()
+			if r[1] & req:
+				r[0](conn)
 
 	def __init__(self):
 		self.routes = {}
@@ -37,12 +38,13 @@ class Context():
 		self.buf_size = buf_size
 		self.conns = {}
 
-def handle_req(ctx: Context, data: str):
+def handle_req(ctx: Context, conn, data: str):
+	print(data)
 	headers = data.split('\r\n')
 	req_header = headers[0].split()
 	req = req_header[0]
 	route = req_header[1]
-	router.handle_route(route, ctx.req_dict[req])
+	router.handle_route(conn, route, ctx.req_dict[req])
 
 def send_response(conn, content):
 	if conn is None:
@@ -58,32 +60,39 @@ def send_response(conn, content):
 def my_func():
 	return "HELLO XD"
 
-def add2buff(buffer, add):
+def add2buff(buffer: str, add: str):
 	buff = buffer + add
 	return (buff, len(buff) >= 4 and buff[-4:] == "\r\n\r\n")
 
-def main():
+if __name__ == "__main__":
 	ctx = Context('127.0.0.1', 5000, 1024)
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	sock.bind((ctx.ip, ctx.port))
 	sock.listen(1)
-	
-	conn, addr = sock.accept()
-	ctx.conns[conn] = ""
-	router.conn = conn
-	print("{} and {}".format(conn.getsockname(), conn.getpeername()))
-	buff = ""
+	sock_list = [sock]
+
 	while True:
-		data = conn.recv(ctx.buf_size)
-		if not data:
-			continue
-		buff, res = add2buff(buff, data.decode('utf-8'))
-		if res:
-			print(len(data))
-			handle_req(ctx, buff)
-			buff = ""
+		read_rdy, write_rdy, err_rdy = select.select(sock_list, [], [])
+		print(read_rdy)
+		for conn in read_rdy:
+			if conn is sock:
+				s, addr = sock.accept()
+				print("{} and {}".format(s.getsockname(), s.getpeername()))
+				ctx.conns[s] = ""
+				sock_list.append(s)
+				continue
+			print(1)
+			data = conn.recv(ctx.buf_size)
+			print(2)
+			if not data:
+				continue
+			ctx.conns[conn], res = add2buff(ctx.conns[conn], data.decode('utf-8'))
+			print(3)
+			print(res)
+			if res:
+				handle_req(ctx, conn, ctx.conns[conn])
+				ctx.conns[conn] = ""
+			print(4)
 	conn.close()
-	
-if __name__ == "__main__":
-	main()
+
